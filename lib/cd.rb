099fafb2c4275864b76b5bb9a3bd3c1141d91ba9
Heather Anderson & Joshua Atteberry <student@epicodus.com>
2015-01-15 14:59:23 -0800

    Add artist and album classes.

--- lib/cd.rb
+++ lib/cd.rb
@@ -1,8 +1,28 @@
 class CD
   attr_reader(:album, :artist)
 
+  @@all_cds = []
+
   define_method(:initialize) do |attributes|
     @album = attributes.fetch(:album)
     @artist = attributes.fetch(:artist)
+  end
+
+  define_singleton_method(:all) do
+    @@all_cds
+  end
+
+  define_method(:save) do
+    @@all_cds.push(self)
+  end
+
+  define_singleton_method(:search) do |search_word|
+    found_album = nil
+    @@all_cds.each() do |album|
+      if album.album() == search_word
+        return found_album = album
+      end
+    end
+    found_album
   end
 end
